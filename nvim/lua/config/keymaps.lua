78b64267f2038c7fe1466e8e473acac2fa0079b7
Huy Lam <commanderh1995@gmail.com>
2023-08-20 21:49:37 -0500

    retracked my nvim folder

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,14 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set(
+  "n",
+  "<leader>sx",
+  require("telescope.builtin").resume,
+  { noremap = true, silent = true, desc = "Resume" }
+)
+
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, nil)
+
+vim.keymap.set("i", "jk", "<ESC>", nil)
